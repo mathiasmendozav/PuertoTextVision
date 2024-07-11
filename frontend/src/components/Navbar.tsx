c0c8c1bdeaaea0ed2557ed21cce10fd7a777dd6a
mathiasmendozav <mathiasmendozavargas1403@gmail.com>
2024-07-11 16:23:44 -0700

    Reloading and Generar Btn

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 // Navbar Component
 import logo from '../../public/logo.jpg'
 import { BsRobot } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
@@ -9,7 +10,9 @@ const Navbar = () => {
                 <img src={logo} alt="Logo" className="h-12 mr-2" />
                 <span className="font-semibold text-xl">PuertoTextVision</span>
             </div>
-            <button className="bg-gradient-to-r from-teal-500 to-blue-500 text-white font-semibold py-2 px-4 flex items-center gap-2 rounded shadow-md"><BsRobot /> Generar Texto</button>
+            <Link to={'/'} onClick={() => { if (window.location.pathname === '/') { window.location.reload(); } }}>
+                <button className="bg-gradient-to-r from-teal-500 to-blue-500 text-white font-semibold py-2 px-4 flex items-center gap-2 rounded shadow-md"><BsRobot /> Generar Texto</button>
+            </Link>
         </nav>
     )
 }
