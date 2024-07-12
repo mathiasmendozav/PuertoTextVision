91782e2593002035197f19959cbc6d42efcf7e33
mathiasmendozav <mathiasmendozavargas1403@gmail.com>
2024-07-11 21:41:29 -0700

    New background design

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import Form from '../components/Form'
 
 const Home = () => {
     return ( 
-        <div className='min-h-screen items-center pt-3 bg-slate-300'>
+        <div className='min-h-screen items-center pt-3 bg-slate-100'>
             <Form />
         </div>
     )
