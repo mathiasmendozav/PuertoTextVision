b5380e95d994809f121729c4a086f11d416a7ecd
mathiasmendozav <mathiasmendozavargas1403@gmail.com>
2024-07-14 12:09:36 -0700

    PostCard component on production

---
+++ frontend/src/components/PostCard.tsx
@@ -0,0 +1,12 @@
+// Post Card Components will go here
+import React from "react";
+
+const PostCard = () => {
+    return (
+        <div className="post-card">
+
+        </div>
+    )
+}
+
+export default PostCard;
