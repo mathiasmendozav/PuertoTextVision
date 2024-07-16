1da5c8ebbab2dd3526d7b2284ab3bb282ac931ee
mathiasmendozav <mathiasmendozavargas1403@gmail.com>
2024-07-16 12:53:52 -0700

    New Bg Theme for main page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,12 @@
 // Home Page
 import Form from '../components/Form'
 
+// Assets
+import HomeBG from '../assets/promocreditoBack.webp'
+
 const Home = () => {
     return ( 
-        <div className='min-h-screen items-center pt-3 bg-slate-100'>
+        <div className='min-h-screen flex items-center justify-center pt-3 bg-slate-100' style={{ backgroundImage: `url(${HomeBG})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
             <Form />
         </div>
     )
