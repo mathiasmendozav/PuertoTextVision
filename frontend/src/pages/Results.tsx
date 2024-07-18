b1190cb3aeb8f40528764ee6fd023061f24a7e7e
mathiasmendozav <mathiasmendozavargas1403@gmail.com>
2024-07-18 11:55:08 -0700

    Testing new Background

--- frontend/src/pages/Results.tsx
+++ frontend/src/pages/Results.tsx
@@ -11,8 +11,8 @@ const Results = () => {
     const { generatedAds } = location.state || { generatedAds: [] };
 
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center pt-3" style={{ backgroundImage: `url(${HomeBG})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-            <div className="mt-10 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+        <div className="min-h-screen flex flex-col items-center justify-center pt-3 bg-gray-400">
+            <div className="my-10 px-6 grid grid-cols-1 gap-7 md:grid-cols-2 lg:grid-cols-3">
                 {generatedAds.map((ad, index) => (
                     <PostCard key={index} text={ad.text} imageUrl={ad.imageUrl} />
                 ))}
