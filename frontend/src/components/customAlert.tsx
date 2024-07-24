bbdd80d038cee29c683d9c6534152c83aba77513
mathiasmendozav <mathiasmendozavargas1403@gmail.com>
2024-07-23 17:58:32 -0700

    Custom Alert done

--- frontend/src/components/customAlert.tsx
+++ frontend/src/components/customAlert.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
+import { GrCopy } from "react-icons/gr";
 
 const CustomAlert = ({ message, isVisible, onClose }) => {
     useEffect(() => {
         if (isVisible) {
             const timer = setTimeout(() => {
                 onClose();
-            }, 3000);
+            }, 50000);
             return () => clearTimeout(timer);
         }
     }, [isVisible, onClose]);
@@ -15,13 +16,37 @@ const CustomAlert = ({ message, isVisible, onClose }) => {
         <AnimatePresence>
             {isVisible && (
                 <motion.div
-                    initial={{ opacity: 0, scale: 0.9 }}
+                    initial={{ opacity: 0, scale: 0.8 }}
                     animate={{ opacity: 1, scale: 1 }}
-                    exit={{ opacity: 0, scale: 0.9 }}
-                    transition={{ duration: 0.3 }}
-                    className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black text-white p-4 rounded-lg shadow-lg z-50"
+                    exit={{ opacity: 0, scale: 0.8 }}
+                    transition={{ duration: 0.5, ease: "easeInOut" }}
+                    className="fixed inset-0 flex items-center justify-center text-center mb-40 z-50"
                 >
-                    {message}
+                    <motion.div
+                        className="bg-[#001540] p-4 w-64 h-42 border-4 border-green-500 rounded-2xl shadow-lg"
+                        initial={{ y: '-50%' }}
+                        animate={{ y: '0%' }}
+                        exit={{ y: '-50%' }}
+                        transition={{ duration: 0.3, ease: "easeInOut" }}
+                    >
+                        <div className="w-full h-full flex flex-col items-center justify-center font-semibold mt-3">
+                            <motion.div
+                                initial={{ scale: 0.9 }}
+                                animate={{ scale: 1.1}}
+                                exit={{ scale: 1 }}
+                                transition={{ 
+                                    duration: 0.5, 
+                                    ease: "easeInOut",
+                                    repeat: 2,
+                                    repeatType: "reverse"
+                                }}
+                            >
+                                <GrCopy className='text-[70px] text-green-500'/>
+                            </motion.div>
+                            <div className='mt-3 text-lg italic'>{message}</div>
+                        </div>
+                        
+                    </motion.div>
                 </motion.div>
             )}
         </AnimatePresence>
