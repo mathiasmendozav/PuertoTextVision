4c743039939086d273a20ad1066e944729ca0a8f
mathiasmendozav <mathiasmendozavargas1403@gmail.com>
2024-07-23 18:36:36 -0700

    Cleaning up code

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -21,7 +21,7 @@ const Home = () => {
     };
 
     return (
-        <div className='min-h-screen flex flex-col items-center justify-center pt-3 bg-slate-100' style={{ backgroundImage: `url(${HomeBG})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+        <div className='min-h-screen flex flex-col items-center justify-center bg-slate-100' style={{ backgroundImage: `url(${HomeBG})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
             {isSubmitted ? (
                 <LoadingSpinner />
             ) : (
