b542a06d9dc4bcd4c8497d3ea344879e2aa4369c
mathiasmendozav <mathiasmendozavargas1403@gmail.com>
2024-07-23 19:01:28 -0700

    New design of main Form

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -35,8 +35,8 @@ const Form = ({ onSubmit }) => {
     };
 
     return (
-        <div className="relative flex items-center justify-center max-sm:mt-[-150px]">
-            <div className="bg-[#001540] p-6 sm:p-8 md:p-10 rounded-md shadow-xl max-w-lg w-full">
+        <div className="bg-[#001540] shadow-xl shadow-gray-700 mb-6 flex items-center justify-center rounded-md max-md:mx-4">
+            <div className="p-6 sm:p-8 md:p-10 shadow-xl max-w-lg">
                 <h2 className="text-2xl sm:text-3xl font-bold text-white mb-6 sm:mb-6 text-center">Formulario de Campaña</h2>
                 <form onSubmit={handleSubmit} className="space-y-4 sm:space-y-6">
                     <div>
