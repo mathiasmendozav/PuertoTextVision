2e9f4ae9e9668efe14336cc7692e66a409f061ac
mathiasmendozav <mathiasmendozavargas1403@gmail.com>
2024-07-25 14:39:06 -0700

    New form UI

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -12,6 +12,8 @@ const Form = ({ onSubmit }) => {
     const [image, setImage] = useState(null);
     const [imagePreview, setImagePreview] = useState(null);
     const [showImageHint, setShowImageHint] = useState(false);
+    const [includePrices, setIncludePrices] = useState(false);
+    const [prices, setPrices] = useState('');
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -19,7 +21,9 @@ const Form = ({ onSubmit }) => {
             campaignTarget,
             promoKeywords,
             textKeywords,
-            image
+            image,
+            includePrices,
+            prices
         });
     };
 
@@ -35,13 +39,13 @@ const Form = ({ onSubmit }) => {
     };
 
     return (
-        <div className="bg-[#001540] shadow-xl shadow-gray-800 mb-6 flex items-center justify-center rounded-md max-md:mx-4">
-            <div className="p-6 sm:p-8 md:p-10 shadow-xl max-w-lg">
-                <h2 className="text-2xl sm:text-3xl font-bold text-white mb-6 sm:mb-6 text-center">Formulario de Campaña</h2>
-                <form onSubmit={handleSubmit} className="space-y-4 sm:space-y-6">
+        <div className="bg-[#001540] shadow-xl shadow-gray-800 mb-6 flex items-center justify-center rounded-md">
+            <div className="w-full p-9 sm:p-8 md:p-10 shadow-xl">
+                <h2 className="text-2xl sm:text-3xl font-bold text-white mb-6 sm:mb-8 text-center">Formulario de Campaña</h2>
+                <form onSubmit={handleSubmit} className="space-y-5">
                     <div>
-                        <label className="block text-white mb-2 sm:mb-4 font-medium">Objetivo de la Campaña</label>
-                        <div className="flex justify-between space-y-2 sm:space-y-0 sm:space-x-4 max-sm:flex-wrap">
+                        <label className="block text-white mb-3 font-medium">Objetivo de la Campaña</label>
+                        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
                             {['Mensajes', 'Ventas', 'Interacción'].map((target) => {
                                 const icons = {
                                     'Mensajes': <TbMessages size={24} />,
@@ -51,7 +55,7 @@ const Form = ({ onSubmit }) => {
                                 return (
                                     <label
                                         key={target}
-                                        className={`flex items-center justify-center cursor-pointer w-full sm:w-1/3 p-2 border border-black rounded-lg shadow-lg transition-transform transform min-[700px]:hover:scale-105 ${campaignTarget === target ? 'bg-gradient-to-r from-teal-500 to-blue-500 text-white' : 'bg-white text-gray-700 hover:bg-gray-200'}`}
+                                        className={`flex items-center justify-center cursor-pointer p-3 border border-black rounded-lg shadow-lg transform transition-transform hover:scale-105 ${campaignTarget === target ? 'bg-gradient-to-r from-teal-500 to-blue-500 text-white' : 'bg-white text-gray-700 hover:bg-gray-200'}`}
                                         onClick={() => setCampaignTarget(target)}
                                     >
                                         <input
@@ -70,9 +74,9 @@ const Form = ({ onSubmit }) => {
                         </div>
                     </div>
                     <div>
-                        <label className="block text-white mb-2 sm:mb-4 font-medium">Subir Imagen</label>
+                        <label className="block text-white mb-3 font-medium">Subir Imagen</label>
                         <div
-                            className="relative w-full h-full min-h-20 border-2 border-dashed border-gray-300 rounded-lg flex items-center justify-center overflow-hidden hover:shadow-lg transition-shadow duration-300"
+                            className="relative w-full h-48 border-2 border-dashed border-gray-300 rounded-lg flex items-center justify-center overflow-hidden hover:shadow-lg transition-shadow duration-300"
                             onMouseEnter={() => setShowImageHint(true)}
                             onMouseLeave={() => setShowImageHint(false)}
                         >
@@ -95,28 +99,68 @@ const Form = ({ onSubmit }) => {
                         </div>
                     </div>
                     <div>
-                        <label className="block text-white mb-2 sm:mb-4 font-medium" htmlFor="promoKeywords">Palabras Clave de la Promoción</label>
+                        <label className="block text-white mb-3 font-medium" htmlFor="promoKeywords">Palabras Clave de la Promoción</label>
                         <input
                             type="text"
                             id="promoKeywords"
-                            className="w-full p-2 sm:p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-500"
+                            className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-500"
                             placeholder="Introduce palabras clave para la promoción"
                             value={promoKeywords}
                             onChange={(e) => setPromoKeywords(e.target.value)}
                         />
                     </div>
                     <div>
-                        <label className="block text-white mb-2 sm:mb-4 font-medium" htmlFor="textKeywords">Palabras Clave del Texto Deseado</label>
+                        <label className="block text-white mb-3 font-medium" htmlFor="textKeywords">Palabras Clave del Texto Deseado</label>
                         <input
                             type="text"
                             id="textKeywords"
-                            className="w-full p-2 sm:p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-500"
+                            className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-500"
                             placeholder="Introduce palabras clave para el texto"
                             value={textKeywords}
                             onChange={(e) => setTextKeywords(e.target.value)}
                         />
                     </div>
-                    <button type="submit" className="w-full flex justify-center gap-2 items-center bg-gradient-to-r from-teal-500 to-blue-500 text-white font-bold py-3 sm:py-4 rounded-lg shadow-lg hover:from-teal-600 hover:to-blue-600 transition duration-300">
+                    <div>
+                        <label className="block text-white mb-3 font-medium">¿Incluir Precios en el Texto?</label>
+                        <div className="flex space-x-4">
+                            <label className="flex items-center">
+                                <input
+                                    type="radio"
+                                    name="includePrices"
+                                    value="yes"
+                                    checked={includePrices === true}
+                                    onChange={() => setIncludePrices(true)}
+                                    className="form-radio text-teal-500 focus:ring-teal-500"
+                                />
+                                <span className="ml-2 text-white">Sí</span>
+                            </label>
+                            <label className="flex items-center">
+                                <input
+                                    type="radio"
+                                    name="includePrices"
+                                    value="no"
+                                    checked={includePrices === false}
+                                    onChange={() => setIncludePrices(false)}
+                                    className="form-radio text-teal-500 focus:ring-teal-500"
+                                />
+                                <span className="ml-2 text-white">No</span>
+                            </label>
+                        </div>
+                    </div>
+                    {includePrices && (
+                        <div>
+                            <label className="block text-white mb-3 font-medium" htmlFor="prices">Introduce los Precios</label>
+                            <input
+                                type="text"
+                                id="prices"
+                                className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-500"
+                                placeholder="Introduce los precios a mencionar"
+                                value={prices}
+                                onChange={(e) => setPrices(e.target.value)}
+                            />
+                        </div>
+                    )}
+                    <button type="submit" className="w-full flex justify-center gap-2 items-center bg-gradient-to-r from-teal-500 to-blue-500 text-white font-bold py-3 rounded-lg shadow-lg hover:from-teal-600 hover:to-blue-600 transition duration-300">
                         <BsRobot />
                         Generar Textos
                     </button>
