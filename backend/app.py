903f4a393911cf9f14e47526a30293dad735c071
mathiasmendozav <mathiasmendozavargas1403@gmail.com>
2024-08-08 20:52:21 -0700

    building app main file for backend

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,13 @@
 # Main Flask App Backend will go here...
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+if __name__  == '__main__':
+    app.run(debug=True)
